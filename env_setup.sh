d24d08e71976f28c7bba5fba2f287fbdd3e24b1e
Yu-Yun Chang <yuyun.chang@hotmail.com>
2023-03-23 03:30:19 +0000

    feat: detect bare metal environment
    - Add a bare-metal check to conditionally apply setup behavior.

--- env_setup.sh
+++ env_setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
+#git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+#sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-[ -f ~/.vim_runtime/my_configs.vim ] && \
-rm ~/.vim_runtime/my_configs.vim
+#[ -f ~/.vim_runtime/my_configs.vim ] && \
+#rm ~/.vim_runtime/my_configs.vim
 
-cat << EOF >> $HOME/.vim_runtime/my_configs.vim
+#cat << EOF >> $HOME/.vim_runtime/my_configs.vim
+cat << EOF >> $HOME/.vimrc
 set nu
 set cursorline
 set tabstop=4
@@ -15,13 +16,18 @@ set t_Co=256
 
 " Color configuration
 set background=dark
+colorscheme elflord
 hi LineNr cterm=bold ctermfg=DarkGrey ctermbg=NONE
 hi CursorLineNr cterm=bold ctermfg=Green ctermbg=NONE
 EOF
 
 [ -d ~/.ssh ] && rm -rf ~/.ssh
-cp -r /workspace/.ssh ~
+if [ -z $USER ]; then
+    cp -r /workspace/.ssh ~
+fi
 
-git clone git@github.com:yuyun-chang/gitstatus.git ~/.local/
+git clone git@github.com:yuyun-chang/gitstatus.git
+[ -d ~/.local ] || mkdir ~/.local
+mv ./gitstatus ~/.local/gitstatus
 cat ~/.local/gitstatus/bashrc.sh >> ~/.bashrc
 exec bash
