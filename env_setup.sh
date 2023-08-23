2c70004c57e4a7a362d111eac832731b648fa02e
Yu-Yun Chang <yuyun.chang@hotmail.com>
2023-08-23 07:33:35 +0000

    feat: use git prompt helper
    - Switch prompt generation to git-prompt.sh.

--- env_setup.sh
+++ env_setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-#git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-#sh ~/.vim_runtime/install_awesome_vimrc.sh
+<<comment
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-#[ -f ~/.vim_runtime/my_configs.vim ] && \
-#rm ~/.vim_runtime/my_configs.vim
+[ -f ~/.vim_runtime/my_configs.vim ] && \
+rm ~/.vim_runtime/my_configs.vim
 
-#cat << EOF >> $HOME/.vim_runtime/my_configs.vim
-[ -f $HOME/.vimrc ] && rm -f $HOME/.vimrc
-cat << EOF >> $HOME/.vimrc
+cat << EOF >> $HOME/.vim_runtime/my_configs.vim
 set nu
 set cursorline
 set tabstop=4
@@ -17,13 +16,28 @@ set t_Co=256
 
 " Color configuration
 set background=dark
-colorscheme elflord
 hi LineNr cterm=bold ctermfg=DarkGrey ctermbg=NONE
 hi CursorLineNr cterm=bold ctermfg=Green ctermbg=NONE
 EOF
+comment
 
-git clone git@github.com:yuyun-chang/gitstatus.git
-[ -d $HOME/.local ] || mkdir $HOME/.local
-mv ./gitstatus $HOME/.local/gitstatus
-cat $HOME/.local/gitstatus/bashrc.sh >> $HOME/.bashrc
-exec bash
+config_file="$HOME/.bashrc"
+git_version="v$(echo $(git --version) | awk '{print $3}')"
+local_dir="$HOME/.local"
+
+[ -d "${local_dir}" ] || mkdir -p "${local_dir}"
+
+pushd .
+git clone https://github.com/git/git.git /tmp/git
+cd /tmp/git
+git checkout "${git_version}"
+mv contrib/completion/git-prompt.sh "${local_dir}"
+popd
+rm -rf /tmp/git
+
+echo "source ~/.local/git-prompt.sh" >> "${config_file}"
+echo "GIT_PS1_SHOWDIRTYSTATE=1" >> "${config_file}"
+echo "PS1='${debian_chroot:+($debian_chroot)}\\[\\033[01;32m\\]\\u@\\h\\[\\033[00m\\] \\[\\033[01;34m\\]\\w \\[\\033[01;36m\\]\$(__git_ps1 \"(%s)\")\\[\\033[00m\\]\\n\\$ '" >> "${config_file}"
+
+source $HOME/.bashrc
+
