63f181579a90eb0544abc1deedd7961e21f43800
Yu-Yun Chang <yuyun.chang@hotmail.com>
2023-08-30 19:17:33 -0700

    fix: keep setup configuration consistent
    - Align generated configuration with the setup script behavior.

--- env_setup.sh
+++ env_setup.sh
@@ -39,5 +39,5 @@ echo "source ~/.local/git-prompt.sh" >> "${config_file}"
 echo "GIT_PS1_SHOWDIRTYSTATE=1" >> "${config_file}"
 echo "PS1='${debian_chroot:+($debian_chroot)}\\[\\033[01;32m\\]\\u@\\h\\[\\033[00m\\] \\[\\033[01;34m\\]\\w \\[\\033[01;36m\\]\$(__git_ps1 \"(%s)\")\\[\\033[00m\\]\\n\\$ '" >> "${config_file}"
 
-source $HOME/.bashrc
+source ${config_file}
 
