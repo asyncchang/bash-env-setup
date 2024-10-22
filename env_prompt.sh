63875936db2f86054f2ce376d4ad34921a622372
Yu-Yun Chang <yuyun.chang@hotmail.com>
2024-10-22 12:31:15 +0800

    feat: add script to parse git status (git_prompt.sh)

--- env_prompt.sh
+++
@@ -1,9 +0,0 @@
-
-# Function to get the current Git branch
-parse_git_branch() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-}
-
-# Set the prompt
-PS1='\[\e[01;32m\]\u@\h\[\e[0m\]:\[\e[01;34m\]\w\[\e[0m\]\[\e[01;33m\]$(parse_git_branch)\[\e[0m\]\n\$ '
-
